4c4bf6ca1318c841ec170c0e4465539504ac3534
01xz <73350430+01xz@users.noreply.github.com>
2022-02-16 00:31:26 +0800

    chore: config verible

--- lua/config/lsp/settings/verible.lua
+++ lua/config/lsp/settings/verible.lua
@@ -1,1 +1,19 @@
-return {}
+return {
+  cmd = {
+    'verible-verilog-ls',
+    '--formal_parameters_indentation',
+    'indent',
+    '--named_parameter_indentation',
+    'indent',
+    '--named_port_indentation',
+    'indent',
+    '--port_declarations_indentation',
+    'indent',
+    '--port_declarations_right_align_packed_dimensions',
+    'true',
+    '--port_declarations_right_align_unpacked_dimensions',
+    'true',
+    '--try_wrap_long_lines',
+    'true',
+  },
+}
